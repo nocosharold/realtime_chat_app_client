68619d2a826b6cb5ce57a0ed8285fafabed2cd4c
nocosharold <nocosharold@gmail.com>
2023-04-17 12:27:42 +0800

    appliend rwd

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://realtime-chat-app-server-y8dt.onrender.com");
+const socket = io.connect("http://localhost:3001");
 
 function App() {
 	const [username, setUsername] = useState("");
